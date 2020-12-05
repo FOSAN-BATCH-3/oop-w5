3c9d525acc6a544a3fa1f171aff8b308dbc02802
Kidou07 <70023129+Kidou07@users.noreply.github.com>
2020-12-05 08:32:53 +0700

    ini kak
    maaf telat ngumpulinnya

--- oop.js
+++ oop.js
@@ -1,4 +1,3 @@
-'use strict'
 /*
   buat lah sebuah program calculator untuk menghitung sebuah perhitungan
   gunakan konsep OOP untuk pengerjaanya 
@@ -12,56 +11,72 @@
 
 
 class Calculator {
-  //write your code here
-  constructor (){
-
-  
-  }
-
-  add () {
-  
-  }
-
-  subtract () {
-   
-  }
-
-  multiply () {
-
-  }
-
-  divide () {
-  
-  }
-
-  square () {
-   
-  }
-  
-  squareRoot () {
- 
-  }
-
-  phi (){
-
-  }
-
-  result(){
-  
+    //write your code here
+    constructor (c){
+  "user strict";
+  this.hasil=c
     
+    }
+  
+    add (a) {
+    this.hasil+= a
+    return this
+    }
+  
+    subtract (a) {
+    this.hasil-= a
+    return this
+    }
+  
+    multiply (a) {
+    this.hasil*= a
+    return this
+    }
+  
+    divide (a) {
+    this.hasil/= a
+    return this
+    }
+  
+    square (a) {
+    this.hasil**=a
+    return this
+    }
+    
+    squareRoot (a) {
+    this.hasil=Math.sqrt(this.hasil)
+    return this
+    }
+  
+    phi (a){
+    this.hasil*=3.14
+    return this
+    }
+  
+    result(){
+    let hasiltotal=this.hasil
+    this.hasil=0
+    if(isNaN(this.hasil)||this.hasil==undefined){
+        this.hasil=0
+        return this
+    }
+    else{
+return hasiltotal
+    }
+    }
   }
-}
-
-/** note : you can use several features from ecmascript, such as:
-* - Classes
-* - Default Parameters
-* - Destructured Assignment
-* - Template Literals
-* - Method Chaining
-*/
-
-const hitung = new Calculator(0)
-
-console.log(hitung.add(3).divide(3).multiply(10).result()) //10
-console.log(hitung.add(3).divide(3).phi().result()) //3.14
-console.log(hitung.add(3).divide(3).squareRoot().result()) // 1 
+  
+  /** note : you can use several features from ecmascript, such as:
+  * - Classes
+  * - Default Parameters
+  * - Destructured Assignment
+  * - Template Literals
+  * - Method Chaining
+  */
+  
+  const hitung = new Calculator(0)
+  
+  console.log(hitung.add(3).divide(3).multiply(10).result()) //10
+  console.log(hitung.add(3).divide(3).phi().result()) //3.14
+  console.log(hitung.add(3).divide(3).squareRoot().result()) // 1 
+  
